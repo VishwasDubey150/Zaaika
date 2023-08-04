d6ad1f2b94bcc0960732eff13c999945bb1061b5
Vishwas <VishwasDubey150@gmail.com>
2023-08-04 14:59:40 +0530

    starting

---
+++ app/src/main/java/com/example/zaaika/splash.kt
@@ -0,0 +1,19 @@
+package com.example.zaaika
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class splash : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        supportActionBar?.hide()
+        Handler().postDelayed(
+            {
+                startActivity(Intent(this@splash,login::class.java))
+            },1000
+        )
+    }
+}
